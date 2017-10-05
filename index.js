a4d73adf13ab3763daedadb2cbc9461f72533634
geoffcraz <geoffcraz@gmail.com>
2017-10-05 16:45:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function happyHolidays() {
-  var message = "Happy holidays!"
-  return (message)
 
-}
-
-function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-}
-
-function holidayCountdown(holiday, days) {
-  return `It's `${days}` days until `${holiday}!``
-
-}
